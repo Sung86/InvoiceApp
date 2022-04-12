b4395e83d28a3b7d49e9086db7947f2e63af2f26
sung <sung.m@tutamail.com>
2022-04-13 02:07:05 +1000

    initial layout - Header & SideBar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
 	<q-layout view="lHh Lpr lFf">
-		<q-page-container>
+		<Header v-if="isSignIn" @toggleSideBar="isShowSideBar = !isShowSideBar" />
+		<SideBarLeft v-if="isSignIn" :show="isShowSideBar" />
+		<q-page-container style="height: 100vh" class="bg-grey-2">
 			<router-view />
 		</q-page-container>
 	</q-layout>
 </template>
+<script>
+import { defineAsyncComponent } from 'vue';
+import { mapGetters } from 'vuex';
+export default {
+	components: {
+		Header: defineAsyncComponent(() => import('@/components/Header')),
+		SideBarLeft: defineAsyncComponent(() => import('@/components/SideBarLeft')),
+	},
+	data: () => ({
+		isShowSideBar: true,
+	}),
+	computed: {
+		...mapGetters({
+			isSignIn: 'user/getIsSignIn',
+		}),
+	},
+};
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,30 @@
 <template>
-	<header>Header</header>
+	<q-header class="bg-white q-py-md">
+		<q-toolbar>
+			<q-btn
+				flat
+				round
+				dense
+				icon="menu"
+				color="black"
+				size="lg"
+				@click="() => $emit('toggleSideBar')"
+			/>
+			<q-space />
+			<div class="text-dark text-h5">Admin</div>
+		</q-toolbar>
+	</q-header>
 </template>
 
 <script>
-export default {};
+export default {
+	emits: {
+		toggleSideBar: {
+			type: Boolean,
+			default: false,
+		},
+	},
+};
 </script>
 
 <style></style>
